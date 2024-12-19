131856eabceb5b7ca3b74ede543e1a4169900c8e
Martin <59419208+xhrudka@users.noreply.github.com>
2024-12-19 12:51:51 +0100

    Update with decoding main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import pandas as pd
-
+import chardet
 # Load CSV into a DataFrame
-df = pd.read_csv(r'C:\Users\marti\OneDrive\Documents\GitHub\CSV_to_PilotTable\csv_import.csv')
+# Simple CSV file
+# df = pd.read_csv(r'C:\Users\Operator\Documents\GIT_Personal\CSV_to_PilotTable-main\CSV_new.csv')
+
+# Complex CSV file
+# Error handler during the opening and decoding of the file
+try:
+    df = pd.read_csv(r'C:\Users\Operator\Documents\GIT_Personal\CSV_to_PilotTable-main\csv_import.csv', encoding='utf-8')
+    print(df.head())
+except Exception as e:
+    print(f"Error: {e}")
 
 # Display the first 5 rows
-print(df.head())
+# print(df.head())
